3f38222e97f676c56c9a1c50a332d4ebc696bc50
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-06-13 19:22:10 +0800

    ClickHouse

---
+++ clickhouse.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+apt install apt-transport-https ca-certificates dirmngr -y || exit $?
+
+GNUPGHOME=$(mktemp -d)
+GNUPGHOME="$GNUPGHOME" gpg --no-default-keyring --keyring /usr/share/keyrings/clickhouse-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 8919F6BD2B48D754
+rm -r "$GNUPGHOME"
+chmod +r /usr/share/keyrings/clickhouse-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/clickhouse-keyring.gpg] https://packages.clickhouse.com/deb stable main" > /etc/apt/sources.list.d/clickhouse.list
+echo -e "Package: *\nPin: origin packages.clickhouse.com\nPin: release o=clickhouse\nPin-Priority: 900" > /etc/apt/preferences.d/99clickhouse
+
+apt update
+apt install clickhouse-server clickhouse-client -y || exit $?
+systemctl enable --now clickhouse-server
+exit 0
--- uninstall.sh
+++ uninstall.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env bash
-apt purge 'nginx*' 'php*' 'mariadb*' --autoremove -y || exit $?
-
+apt purge 'nginx*' --autoremove -y
 rm -f /etc/apt/sources.list.d/nginx.list
 rm -f /etc/apt/preferences.d/99nginx
 rm -f /usr/share/keyrings/nginx-archive-keyring.gpg
 
+apt purge 'php*' --autoremove -y
 rm -f /etc/apt/sources.list.d/php.list
 rm -f /etc/apt/preferences.d/99php
 rm -f /etc/apt/trusted.gpg.d/php.gpg
 
+apt purge 'mariadb*' --autoremove -y
 rm -f /etc/apt/sources.list.d/mariadb.list
 rm -f /etc/apt/preferences.d/99mariadb
 rm -f /etc/apt/keyrings/mariadb-keyring.pgp
 
 systemctl disable --now mariadb
-rm -fr /data/mariadb
-rm -fr /usr/local/mariadb
 rm -f /etc/my.cnf
 rm -f /etc/systemd/system/mariadb.service
+rm -fr /data/mariadb
+rm -fr /usr/local/mariadb
 userdel -fr mariadb
 systemctl daemon-reload
+
+systemctl disable --now clickhouse-server
+apt purge clickhouse-server clickhouse-client --autoremove -y
+rm -f /etc/apt/sources.list.d/clickhouse.list
+rm -f /etc/apt/preferences.d/99clickhouse
+rm -f /etc/security/limits.d/clickhouse.conf
+rm -f /usr/share/keyrings/clickhouse-keyring.gpg*
+rm -fr /var/lib/clickhouse
+rm -fr /var/log/clickhouse-server
 
 apt update
 exit 0
