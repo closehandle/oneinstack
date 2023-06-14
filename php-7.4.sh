3b6e95e60a66db36634e76cbeb96f772c8ac5b02
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-06-15 05:28:13 +0800

    Update PHP

--- php-7.4.sh
+++ php-7.4.sh
@@ -9,18 +9,25 @@ apt install php7.4-fpm -y || exit $?
 
 sed -E -i 's|^;date.timezone =.*|date.timezone = Asia/Shanghai|g' /etc/php/7.4/cli/php.ini
 sed -E -i 's|^;date.timezone =.*|date.timezone = Asia/Shanghai|g' /etc/php/7.4/fpm/php.ini
-sed -E -i 's|^post_max_size = .*|post_max_size = 1024M|' /etc/php/7.4/cli/php.ini
-sed -E -i 's|^post_max_size = .*|post_max_size = 1024M|' /etc/php/7.4/fpm/php.ini
+sed -E -i 's|^post_max_size = .*|post_max_size = 1024M|g' /etc/php/7.4/cli/php.ini
+sed -E -i 's|^post_max_size = .*|post_max_size = 1024M|g' /etc/php/7.4/fpm/php.ini
 sed -E -i 's|^upload_max_filesize = .*|upload_max_filesize = 1024M|' /etc/php/7.4/cli/php.ini
 sed -E -i 's|^upload_max_filesize = .*|upload_max_filesize = 1024M|' /etc/php/7.4/fpm/php.ini
+sed -E -i 's|^short_open_tag = Off|short_open_tag = On|g' /etc/php/7.4/cli/php.ini
+sed -E -i 's|^short_open_tag = Off|short_open_tag = On|g' /etc/php/7.4/fpm/php.ini
+sed -E -i 's|^expose_php = On|expose_php = Off|g' /etc/php/7.4/cli/php.ini
+sed -E -i 's|^expose_php = On|expose_php = Off|g' /etc/php/7.4/fpm/php.ini
 
 cat <<EOF>/etc/php/7.4/fpm/pool.d/www.conf
 [www]
 user = www
 group = www
 listen = /run/php/php7.4-fpm.sock
+listen.backlog = -1
+listen.allowed_clients = 127.0.0.1
 listen.owner = www
 listen.group = www
+listen.mode = 0666
 
 pm = dynamic
 pm.max_children = 8
@@ -28,6 +35,8 @@ pm.start_servers = 8
 pm.min_spare_servers = 8
 pm.max_spare_servers = 8
 pm.max_requests = 128
+request_terminate_timeout = 30
+request_slowlog_timeout = 0
 EOF
 
 systemctl daemon-reload
