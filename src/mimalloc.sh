bcd05474e75c1cd2b905545a015ddb9b5b2a952e
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-06-29 19:28:56 +0800

    Add files via upload

--- src/mimalloc.sh
+++ src/mimalloc.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 apt update || exit $?
-apt install libmimalloc2.0 -y || exit $?
+apt install build-essential cmake -y || exit $?
 
-echo '/usr/lib/x86_64-linux-gnu/libmimalloc.so.2.0' > /etc/ld.so.preload
+pushd /opt
+rm -fr mimalloc
+git clone https://github.com/microsoft/mimalloc -b v2.1.2 --depth 1 --single-branch mimalloc || exit $?
+cd mimalloc && mkdir release && cd release
+
+cmake -DCMAKE_BUILD_TYPE='RelWithDebInfo' .. || exit $?
+cmake --build . -j$(nproc) || exit $?
+
+cp -f libmimalloc.so.2.1 /usr/local/lib
+cd ../.. && rm -fr mimalloc
+popd
+
+# echo '/usr/local/lib/libmimalloc.so.2.1' > /etc/ld.so.preload
 ldconfig
 exit 0
