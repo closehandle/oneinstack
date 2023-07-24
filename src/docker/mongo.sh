65a1dca3e48a10f062b504d4b5dae4803d7d8d5f
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-07-25 05:51:22 +0800

    Update docker mongo script

--- src/docker/mongo.sh
+++ src/docker/mongo.sh
@@ -8,6 +8,8 @@ docker run \
     --env MONGO_INITDB_ROOT_PASSWORD=$UUID \
     --name mongo \
     --detach \
+    --volume mongo-etcs:/etc/mongo \
+    --volume mongo-data:/data/db \
     --restart always \
     --network host \
     mongo:latest || exit $?
