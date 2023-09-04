bdde86ded54fc827de450281f2d0e4a9c5e61755
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-09-05 02:58:29 +0800

    Fix nginx.sh

--- create.sh
+++ create.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 cd $(dirname "$0")
+apt install jq -y || exit $?
+
 mkdir -p /etc/docker
 cp -f etc/docker/daemon.json /etc/docker/daemon.json
 systemctl restart docker
@@ -15,19 +17,20 @@ mkdir -p /data/wwwroot/default
 
 clear
 sleep 3
-echo '[+] Subnet      :' $(docker network inspect oneinstack | jq -r '.[0].IPAM.Config[0].Subnet')
-echo '[+] Web Server  :' $(docker container inspect nginx | jq -r '.[0].NetworkSettings.Networks.oneinstack.IPAddress')
-echo '[+] DB Hostname :' $(docker container inspect mariadb | jq -r '.[0].NetworkSettings.Networks.oneinstack.IPAddress')
+SUBNET=$(docker network inspect oneinstack | jq -r '.[0].IPAM.Config[0].Subnet')
+NGINXIP=$(docker container inspect nginx | jq -r '.[0].NetworkSettings.Networks.oneinstack.IPAddress')
+MARIADBIP=$(docker container inspect mariadb | jq -r '.[0].NetworkSettings.Networks.oneinstack.IPAddress')
+echo '[+] Subnet      :' $SUBNET
+echo '[+] Web Server  :' $NGINXIP
+echo '[+] DB Hostname :' $MARIADBIP
 echo '[+] DB Password'   $(docker container logs mariadb 2>&1 | grep 'GENERATED ROOT PASSWORD' | awk -F'GENERATED ROOT PASSWORD' '{print $2}')
 echo
 echo "[+] By default, docker's manipulation of iptables is disabled."
 echo "[+] So, don't forget to manually configure the iptables NAT rules!"
-echo '[+] iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -j MASQUERADE'
-echo '[+] Replace 10.0.0.0/8 with the subnet address output above'
+echo "[+] iptables -t nat -A POSTROUTING -s $SUBNET -j MASQUERADE"
 echo
 echo '[+] The nginx ports are not exposed by default, you need to configure iptables DNAT manually.'
-echo '[+] iptables -t nat -A PREROUTING -d 1.1.1.1/32 -p tcp -m tcp --dport 80 -j DNAT --to-destination 10.0.0.100:80'
-echo '[+] iptables -t nat -A PREROUTING -d 1.1.1.1/32 -p tcp -m tcp --dport 443 -j DNAT --to-destination 10.0.0.100:443'
+echo "[+] iptables -t nat -A PREROUTING -d 1.1.1.1/32 -p tcp -m tcp --dport 80 -j DNAT --to-destination $NGINXIP:80"
+echo "[+] iptables -t nat -A PREROUTING -d 1.1.1.1/32 -p tcp -m tcp --dport 443 -j DNAT --to-destination $NGINXIP:443"
 echo "[+] Replace 1.1.1.1 with your server's IP address"
-echo '[+] Replace 10.0.0.100 with the web server address output above'
 exit 0
--- src/nginx/create.sh
+++ src/nginx/create.sh
@@ -2,30 +2,45 @@
 cd $(dirname "$0")
 docker pull nginx:alpine || exit $?
 
-if [[ ! -f default.crt ]]; then
-    IP=$(curl -4fsSL ip.sb)
-    openssl req -x509 -newkey ec:<(openssl ecparam -name secp384r1) -sha384 -days 3650 -nodes \
-        -keyout default.key -out default.crt -subj "/CN=${IP}" \
-        -addext "subjectAltName=IP:${IP}"
-    openssl dhparam -out ffdhe2048.txt 2048
+docker container run \
+    --rm \
+    --env TZ=Asia/Shanghai \
+    --tty \
+    --name nginx \
+    --volume /etc:/opt \
+    --hostname nginx \
+    --interactive \
+    nginx:alpine \
+    cp -fr /etc/nginx /opt
 
-    chmod 0644 default.crt default.key
-fi
+IP=$(curl -4fsSL ip.sb)
+openssl req -x509 -newkey ec:<(openssl ecparam -name secp384r1) -sha384 -days 3650 -nodes \
+    -keyout default.key -out default.crt -subj "/CN=${IP}" \
+    -addext "subjectAltName=IP:${IP}"
+openssl dhparam -out ffdhe2048.txt 2048
+chmod 0644 default.crt default.key
 
-docker build -t nginx:customized . || exit $?
-
-rm -f default.crt
-rm -f default.key
-rm -f ffdhe2048.txt
+mv -f  ffdhe2048.txt /etc/nginx
+cp -f  fastcgi.conf  /etc/nginx
+cp -f  nginx.conf    /etc/nginx
+cp -f  vhost.conf    /etc/nginx
+cp -fr rewrite       /etc/nginx/rewrite
+cp -fr ssl           /etc/nginx/ssl
+mv -f  default.crt   /etc/nginx/ssl/default.crt
+mv -f  default.key   /etc/nginx/ssl/default.key
+rm -fr /etc/nginx/conf.d
+rm -fr /etc/nginx/modules
+rm -f  /etc/nginx/*_params
 
 docker container run \
     --env TZ=Asia/Shanghai \
     --name nginx \
     --detach \
+    --volume /etc/nginx:/etc/nginx \
     --volume /data/wwwlogs:/data/wwwlogs \
     --volume /data/wwwroot:/data/wwwroot \
     --network oneinstack \
     --restart always \
     --hostname nginx \
-    nginx:customized
+    nginx:alpine
 exit 0
