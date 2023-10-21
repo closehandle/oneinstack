ee93a43bc8f1c26b435f59dd022a120e50a38529
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-10-21 23:46:52 +0800

    修复 ClickHouse 的 NOFILE 参数

--- src/clickhouse/create.sh
+++ src/clickhouse/create.sh
@@ -8,7 +8,7 @@ docker container run \
     --name clickhouse \
     --detach \
     --volume clickhouse:/var/lib/clickhouse \
-    --ulimit NOFILE=262144:262144 \
+    --ulimit nofile=262144:262144 \
     --network oneinstack \
     --restart always \
     --hostname clickhouse \
