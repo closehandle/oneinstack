f5d8ca1f993d85810418394f3facfacb81695971
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-12-03 22:48:18 +0800

    优化安装脚本

--- create.sh
+++ create.sh
@@ -13,24 +13,8 @@ mkdir -p /data/wwwroot/default
 
 clear
 sleep 3
-SUBNET=$(docker network inspect oneinstack | jq -r '.[0].IPAM.Config[0].Subnet')
-NGINXIP=$(docker container inspect nginx | jq -r '.[0].NetworkSettings.Networks.oneinstack.IPAddress')
-MARIADBIP=$(docker container inspect mariadb | jq -r '.[0].NetworkSettings.Networks.oneinstack.IPAddress')
-echo '[+] Subnet      :' $SUBNET
-echo '[+] Web Server  :' $NGINXIP
-echo '[+] DB Hostname :' $MARIADBIP
+echo '[+] Subnet      : 192.168.88.0/24'
+echo '[+] Web Server  : 192.168.88.100'
+echo '[+] DB Hostname : 192.168.88.254'
 echo '[+] DB Password'   $(docker container logs mariadb 2>&1 | grep 'GENERATED ROOT PASSWORD' | awk -F'GENERATED ROOT PASSWORD' '{print $2}')
-echo
-echo "[+] By default, docker's manipulation of iptables is disabled."
-echo "[+] So, don't forget to manually configure the iptables NAT rules!"
-echo "[+] iptables -t nat -A POSTROUTING -s $SUBNET -j MASQUERADE"
-echo
-echo '[+] The nginx ports are not exposed by default, you need to configure iptables DNAT manually.'
-echo "[+] iptables -t nat -A PREROUTING -d 1.1.1.1/32 -p tcp -m tcp --dport 80 -j DNAT --to-destination $NGINXIP:80"
-echo "[+] iptables -t nat -A PREROUTING -d 1.1.1.1/32 -p tcp -m tcp --dport 443 -j DNAT --to-destination $NGINXIP:443"
-echo "[+] Replace 1.1.1.1 with your server's IP address"
-
-mkdir -p /etc/docker
-cp -f etc/docker/daemon.json /etc/docker/daemon.json
-systemctl restart docker
 exit 0
