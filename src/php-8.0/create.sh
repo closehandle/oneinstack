e8c1d39535ae7702c3866d8cd5c495ae6c3fbfc7
CloseHandle <88403145+closehandle@users.noreply.github.com>
2024-01-16 05:54:57 +0800

    增加 PHP 8.0 和 8.1

---
+++ src/php-8.0/create.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+docker container rm -f php-fpm > /dev/null 2>&1
+
+docker container run \
+    --ip 192.168.88.101 \
+    --env TZ=Asia/Shanghai \
+    --name php-fpm \
+    --pull always \
+    --detach \
+    --volume /data/wwwroot:/data/wwwroot \
+    --network oneinstack \
+    --restart always \
+    --hostname php-fpm \
+    closehandle/php:8.0-fpm-alpine
+exit $?
---
+++ src/php-8.1/create.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+docker container rm -f php-fpm > /dev/null 2>&1
+
+docker container run \
+    --ip 192.168.88.101 \
+    --env TZ=Asia/Shanghai \
+    --name php-fpm \
+    --pull always \
+    --detach \
+    --volume /data/wwwroot:/data/wwwroot \
+    --network oneinstack \
+    --restart always \
+    --hostname php-fpm \
+    closehandle/php:8.1-fpm-alpine
+exit $?
