20e1c3e8dff7ba3323253c905eb6e8b3ce232d15
CloseHandle <88403145+closehandle@users.noreply.github.com>
2024-05-04 03:50:09 +0800

    增加 MongoDB 6.0 支持

---
+++ src/mongodb-6.0/create.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+docker container rm -f mongodb > /dev/null 2>&1
+
+docker container run \
+    --ip 192.168.88.251 \
+    --env TZ=Asia/Shanghai \
+    --env MONGO_INITDB_ROOT_USERNAME=admin \
+    --env MONGO_INITDB_ROOT_PASSWORD=oneinstack \
+    --name mongodb \
+    --pull always \
+    --detach \
+    --volume mongodb:/data/db \
+    --network oneinstack \
+    --restart always \
+    --hostname mongodb \
+    mongo:6.0
+exit $?
