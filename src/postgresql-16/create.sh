8a44dd8d43968b9be8107bf35e21fcd2a463c164
CloseHandle <88403145+closehandle@users.noreply.github.com>
2024-05-04 03:52:19 +0800

    增加 PostgreSQL 16 支持

---
+++ src/postgresql-16/create.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+docker container rm -f postgresql > /dev/null 2>&1
+
+docker container run \
+    --ip 192.168.88.253 \
+    --env TZ=Asia/Shanghai \
+    --env POSTGRES_PASSWORD=oneinstack \
+    --name postgresql \
+    --pull always \
+    --detach \
+    --volume postgresql:/var/lib/postgresql/data \
+    --network oneinstack \
+    --restart always \
+    --hostname postgresql \
+    postgres:16
+exit $?
