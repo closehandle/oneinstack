d5e359e3216a4bf5019bd224b7cf8c28343424ea
CloseHandle <88403145+closehandle@users.noreply.github.com>
2024-05-19 05:37:36 +0800

    增加 Redis 7.0 支持

---
+++ src/redis-7.0/create.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+docker container rm -f redis > /dev/null 2>&1
+
+docker container run \
+    --ip 192.168.88.250 \
+    --env TZ=Asia/Shanghai \
+    --name redis \
+    --pull always \
+    --detach \
+    --network oneinstack \
+    --restart always \
+    --hostname redis \
+    redis:7.0-alpine
+exit $?
