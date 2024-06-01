f7561acf91211036ba79b178f0dca7287e0935ca
CloseHandle <88403145+closehandle@users.noreply.github.com>
2024-06-01 14:23:13 +0800

    增加 MariaDB 11.4 支持

---
+++ src/mariadb-11.4/create.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+docker container rm -f mariadb > /dev/null 2>&1
+
+docker container run \
+    --ip 192.168.88.254 \
+    --env TZ=Asia/Shanghai \
+    --env MARIADB_RANDOM_ROOT_PASSWORD=yes \
+    --name mariadb \
+    --pull always \
+    --detach \
+    --volume mariadb:/var/lib/mysql \
+    --network oneinstack \
+    --restart always \
+    mariadb:11.4
+exit $?
