7a5def53d20ac0ba7f96a2dc60c46b3b598d5814
CloseHandle <88403145+closehandle@users.noreply.github.com>
2024-06-01 14:30:32 +0800

    更新清理脚本

--- delete.sh
+++ delete.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker container rm -fv nginx
 docker container rm -fv mariadb
-docker container rm -fv php-fpm
+docker container rm -fv php-8.3-fpm
 docker network prune -f
 docker image prune -af
 
